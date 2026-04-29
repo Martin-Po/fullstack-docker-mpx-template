07b6e73950e551d3f2b61e862fc8b6aa88ed6236
Martin-Po <85139462+Martin-Po@users.noreply.github.com>
2026-04-29 00:48:25 -0300

    Defined port 3001 for backend

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -10,7 +10,7 @@ function safeRead(path) {
     }
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 const DB_HOST = process.env.DB_HOST || 'localhost';
